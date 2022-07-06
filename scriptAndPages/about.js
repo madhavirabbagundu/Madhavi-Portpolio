3e7e23dd4c0f67bcc27ff875feb6fd8bee1f9620
madhavirabbagungu@gmail.com <madhavirabbgundu@gmail.com>
2022-07-06 19:02:18 +0530

    portfolio

---
+++ scriptAndPages/about.js
@@ -0,0 +1,35 @@
+let about = document.getElementById("about")
+about.innerHTML = `
+<section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/madhaviphoto2 (2).jpg" alt=""
+                        style="border-radius:250px;background-color: rgba(146, 139, 139, 0.068);">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Madhavi Rabbagundu and I'm a <span class="typing-2"></span></div>
+                    <p>Aspiring Full Stack Web Developer and capable of writing efficient code using
+                        MERN Stack. Ambitious to work and collaborate with other dedicated and enterprising developers.
+                        <br>
+                        Eager to take up a responsible role within an exciting and inspiring organization, and deliver
+                        products that offer a great user experience.
+                    </p>
+                    <div class="mediaLinksAbout">
+                        <a href="https://www.linkedin.com/in/madhavi-rabbagundu-191225231/" target="_blank">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                        <a href="https://github.com/madhavirabbagundu" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </div>
+                    <p class="iconsAboutSection"> <i class="fas fa-phone"></i> +91-6366908401</p>
+                    <p class="iconsAboutSection"><i class="fas fa-envelope"></i> madhavirabbagundu@gmail.com</p>
+                    <a href="https://drive.google.com/file/d/1mxP6q88CIjUC1hj8_shaxZDekzrAjB6a/view?usp=sharing"
+                        target="_blank" class="resumeToDrive">Resume</a>
+                </div>
+            </div>
+        </div>
+    </section>
+`;
---
+++ scriptAndPages/projects.js
@@ -0,0 +1,71 @@
+let projectsSection = document.getElementById("mainDivProjectsSection");
+projectsSection.innerHTML = `
+            <!-- product hunt -->
+            <div class="eachProjectDiv MailChimp">
+                <p class="ProjectName ">Dineout-Clone</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                         <img src="./images/Screenshot (408).png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                    <p>Developed a clone of <a href="https://dineoutproject.netlify.app/"
+                    style="color: #FFF; text-decoration: underline;" target="_blank">Dineout</a> website.<p >
+                        <br >
+                        Along with 3 other members, build the application in 7 days. each individual page was assigned by individual person. websites which we includes Login page, Home page & Products page.</p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                        <a href="https://github.com/madhavirabbagundu/Dineout-second-project" target="_blank"><button>
+                        Github</button></a>
+                        <a href="https://dineoutproject.netlify.app/" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <!-- Apollo project -->
+            <div class="eachProjectDiv ">
+                <p class="ProjectName ">Apollo247</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/project_image.png" alt="" />
+                    </div>
+                     <div class="projectDescription">
+                        <p>Developed a clone of <a href="https://project-madhavi.netlify.app"
+                                style="color: #FFF; text-decoration: underline;" target="_blank">Apollo247</a> website.</p>
+                        <br >
+                        <p>it is a team project, we are four developers to build this project in 7 days . websites which we includes the features Login page, Cart page, Appointment page, blog page and doctors page.</p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/madhavirabbagundu/apolloclone-project"
+                                target="_blank"><button>
+                                    Github</button></a>
+                            <a href="https://project-madhavi.netlify.app" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        
+           
+
+
+`;
