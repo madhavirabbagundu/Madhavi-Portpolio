3f463b032079267e115471e52ec20287b8a68d25
madhavirabbagungu@gmail.com <madhavirabbgundu@gmail.com>
2022-07-22 00:17:08 +0530

    changes

--- scriptAndPages/projects.js
+++ scriptAndPages/projects.js
@@ -8,7 +8,7 @@ projectsSection.innerHTML = `
                          <img src="./images/Screenshot (408).png" alt="" />
                     </div>
                     <div class="projectDescription">
-                    <p>Developed a clone of <a href="https://dineoutproject.netlify.app/"
+                    <p>Developed a clone of <a href="https://dineoutclone-project.netlify.app/"
                     style="color: #FFF; text-decoration: underline;" target="_blank">Dineout</a> website.<p >
                         <br >
                         Along with 3 other members, build the application in 7 days. each individual page was assigned by individual person. websites which we includes Login page, Home page & Products page.</p>
@@ -24,7 +24,7 @@ projectsSection.innerHTML = `
                         <div class="ProjectLinks">
                         <a href="https://github.com/madhavirabbagundu/Dineout-second-project" target="_blank"><button>
                         Github</button></a>
-                        <a href="https://dineoutproject.netlify.app/" target="_blank"><button>Demo</button></a>
+                        <a href="https://dineoutclone-project.netlify.app/" target="_blank"><button>Demo</button></a>
 
                         </div>
                     </div>
@@ -64,6 +64,39 @@ projectsSection.innerHTML = `
                 </div>
             </div>
 
+
+            
+
+            <div class="eachProjectDiv ">
+            <p class="ProjectName ">Trivago</p>
+            <div class="ProjectContainer">
+                <div class="ProjectImage">
+                    <img src="./images/Screenshot (457).png" alt="" />
+                </div>
+                 <div class="projectDescription">
+                    <p>Developed a clone of <a href="https://trivagoclone-project.netlify.app/"
+                            style="color: #FFF; text-decoration: underline;" target="_blank">Apollo247</a> website.</p>
+                    <br >
+                    <p>it is a team project, we are four developers to build this project in 5 days . websites which we includes the features Login page, Home page, hotel page and gallery page.</p>
+                    <br>
+                    <u style="text-decoration: underline;">TechStack :- </u>
+                    <br>
+                    <div class="techStackProject">
+                        <p><i class="fab fa-react"></i> React</p>
+                        <p><i class="fab fa-react"></i>Redux</p>
+                        
+                        
+                    </div>
+                    <div class="ProjectLinks">
+                        <a href="https://github.com/madhavirabbagundu/3rd-project"
+                            target="_blank"><button>
+                                Github</button></a>
+                        <a href="https://trivagoclone-project.netlify.app/" target="_blank"><button>Demo</button></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
         
            
 
